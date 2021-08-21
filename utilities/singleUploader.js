0bb3fa5014314eb7f3f405a6f5c28ab35ed199dc
AP-Porag <smauhp@gmail.com>
2021-08-21 21:20:47 +0600

    path Changed

--- utilities/singleUploader.js
+++ utilities/singleUploader.js
@@ -7,7 +7,7 @@ const createError = require("http-errors");
 
 function uploader(subfolder_path, allowed_file_types, max_file_size, error_msg) {
     //file upload folder
-    const UPLOADS_FOLDER = `${__dirname}/../public/uploads/${subfolder_path}/`;
+    const UPLOADS_FOLDER = `${__dirname}/../uploads/${subfolder_path}/`;
     //define the storage
     const storage = multer.diskStorage({
         destination: (req, file, cb) => {
